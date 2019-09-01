f6be984d035e718241c32f43b052c47938d4e459
Viktor Koves <3187531+vkoves@users.noreply.github.com>
2019-09-01 17:30:46 -0500

    Added HTML Proofer with Travis Config and separate ci.sh This is based on my personal website's setup

--- README.md
+++ README.md
@@ -13,6 +13,16 @@ jekyll serve
 ```
 The website will be locally available at [localhost:400][localhost-4000].
 
+## Running CI
+
+This project has continuous integration setup through Travis, and the
+actual tests executed (HTML Proofer primarily) are run through `ci.sh`. To
+run this, `cd` into the project folder and run:
+
+```shell
+./ci.sh
+```
+
 
 <!-- Links shorthands -->
 [jekyll-home]:      https://jekyllrb.com/
---
+++ ci.sh
@@ -0,0 +1,13 @@
+# Continuous Integration - run by Travis or can be run locally
+
+# Run Jekyll build to generate site output
+jekyll build
+
+# Run HTML Proofer with the following parameters:
+#
+# --assume-extension			- Fix needing the .html extension on links
+# --check_html					- Check for HTML errors using Nokogiri
+# --enforce_https				- Enforce HTTPS (since hosted on HTTPS)
+#
+# Learn more: https://github.com/gjtorikian/html-proofer#configuration
+htmlproofer ./_site --assume-extension --check-html --enforce_https
